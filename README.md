da96286c7bad7748618bd05d5e4a284de1976cce
Franklin Cruz <f.cruz@tvup.media>
2023-01-25 10:24:49 -0300

    feat: Support ping command

--- README.md
+++ README.md
@@ -32,3 +32,11 @@ Note: This section is for stages 2 and beyond.
    in `app/server.go`.
 1. Commit your changes and run `git push origin master` to submit your solution
    to CodeCrafters. Test output will be streamed to your terminal.
+
+# Debug utilities
+
+Dump port data:
+
+```bash
+sudo tcpdump -i lo0 -X port 6379 or port 6380
+```
--- app/server.go
+++ app/server.go
@@ -1,21 +1,173 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	fmt.Println("Starting redis server")
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+// Parser
+
+func readUntilTerminator(reader *bufio.Reader) ([]byte, error) {
+	var last byte
+	var bytes []byte
+	for {
+		byte, err := reader.ReadByte()
+		if err != nil {
+			last = 0
+			return nil, err
+		}
+		bytes = append(bytes, byte)
+		if last == '\r' && byte == '\n' {
+			return bytes[:len(bytes)-2], nil
+		}
+		last = byte
+	}
+}
+
+func readTerminator(reader *bufio.Reader) error {
+	// Terminator should be \r\n
+	last, err := reader.ReadByte()
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		last = 0
+		return err
+	}
+	if last != '\r' {
+		return errors.New(fmt.Sprintf("unexpected input byte: %02x", last))
+	}
+	last, err = reader.ReadByte()
+	if err != nil {
+		last = 0
+		return err
+	}
+	if last != '\n' {
+		return errors.New(fmt.Sprintf("unexpected input byte: %02x", last))
+	}
+
+	return nil
+}
+
+func bulkStr(reader *bufio.Reader) (string, error) {
+	var bytes []byte
+
+	bytes, err := readUntilTerminator(reader)
+	if err != nil {
+		return "", err
+	}
+
+	length, err := strconv.Atoi(string(bytes))
+	if err != nil {
+		return "", err
+	}
+
+	// Handle null bulk string (length == -1)
+
+	str, err := readUntilTerminator(reader)
+	if err != nil {
+		return "", err
+	}
+
+	if len(str) != int(length) {
+		return "", errors.New("bulk string length does not match expected")
+	}
+
+	return string(str), nil
+}
+
+func array(reader *bufio.Reader) ([]interface{}, error) {
+	var bytes []byte
+
+	bytes, err := readUntilTerminator(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	length, err := strconv.Atoi(string(bytes))
+	if err != nil {
+		return nil, err
+	}
+
+	output := make([]interface{}, int(length))
+	for i := 0; i < int(length); i++ {
+		next, err := readInput(reader)
+		if err != nil {
+			return nil, err
+		}
+		output[i] = next
+	}
+
+	return output, nil
+}
+
+func readInput(reader *bufio.Reader) (interface{}, error) {
+	fmt.Println("Reading input...")
+	dataType, err := reader.ReadByte()
+
+	if err != nil {
+		fmt.Println("Failed to read data type: ", err)
+		return nil, err
+	}
+
+	fmt.Println("Reading...", string(dataType))
+	switch dataType {
+	case '+':
+		return nil, errors.New("simple strings not implemented")
+	case '-':
+		return nil, errors.New("errors not implemented")
+	case ':':
+		return nil, errors.New("integers not implemented")
+	case '$':
+		return bulkStr(reader)
+	case '*':
+		return array(reader)
+	default:
+		return nil, errors.New(fmt.Sprintf("unsupported data type symbol: %d", dataType))
+	}
+}
+
+// Commands
+
+func executeCmd(cmd interface{}, conn net.Conn) {
+	if cmdArr, ok := cmd.([]interface{}); ok {
+		if cmdStr, ok := cmdArr[0].(string); ok {
+			cmdStr = strings.ToUpper(cmdStr)
+			fmt.Println("Command is: ", cmdStr)
+			switch cmdStr {
+			case "PING":
+				conn.Write([]byte{'+', 'P', 'O', 'N', 'G', '\r', '\n'})
+			}
+		}
+	}
+}
+
+// Server
+
+func main() {
+	PORT := "6379"
+	fmt.Println("Starting redis server")
+	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", PORT))
+	if err != nil {
+		fmt.Println("Failed to bind to port: ", PORT)
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	input, err := readInput(reader)
+
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	executeCmd(input, conn)
 }
